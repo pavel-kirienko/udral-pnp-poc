886d7d409da22850934950be22fd0dec403fa205
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-06-17 18:45:31 +0300

    Refactor the service detector

--- service_detector.py
+++ service_detector.py
@@ -1,1 +1,110 @@
- 
+# Pavel Kirienko <pavel@uavcan.org>
+
+from __future__ import annotations
+import dataclasses
+from typing import Iterable, Sequence, Tuple
+
+
+@dataclasses.dataclass(frozen=True)
+class PortSuffixMapping:
+    """
+    If the full port name follows the pattern ``[service_name.[instance_name.]]suffix``,
+    where the bracketed part is optional, this type provides the mapping from ``suffix`` to the full port name.
+    """
+    pub: dict[str, str] = dataclasses.field(default_factory=dict)
+    sub: dict[str, str] = dataclasses.field(default_factory=dict)
+    cln: dict[str, str] = dataclasses.field(default_factory=dict)
+    srv: dict[str, str] = dataclasses.field(default_factory=dict)
+
+
+# pytest --doctest-modules -sv service_detector.py
+def detect_service_instances(pub: Iterable[str] = (),
+                             sub: Iterable[str] = (),
+                             cln: Iterable[str] = (),
+                             srv: Iterable[str] = ()) -> dict[str, dict[str, PortSuffixMapping]]:
+    """
+    Detects service instances provided by a remote node from the names of its ports
+    (publishers, subscribers, clients, and servers).
+
+    Here's a demo; suppose that the remote node has the following non-fixed ports:
+
+    >>> pub = [
+    ...     "airspeed.foo.differential_pressure",   # Service "airspeed", instance "foo"
+    ...     "airspeed.foo.static_air_temperature",  # Service "airspeed", instance "foo"
+    ...     "airspeed.bar.differential_pressure",   # Service "airspeed", instance "bar"
+    ...     "servo.feedback",                       # Service "servo", anonymous instance (singleton)
+    ...     "servo.status",                         # etc.
+    ...     "servo.power",
+    ...     "servo.dynamics",
+    ... ]
+    >>> sub = [
+    ...     "airspeed.bar.heater.state",            # Service "airspeed", instance "bar" (see above)
+    ...     "servo.setpoint",                       # Service "servo", anonymous instance (see above)
+    ...     "servo.readiness",
+    ...     "unrelated.subscription",               # Application-specific or vendor-specific subject, non-standard
+    ... ]
+    >>> srv = [
+    ...     "unrelated.server",                     # Application-specific or vendor-specific server, non-standard
+    ...     "standalone_server",                    # Not part of a service, non-standard
+    ... ]
+
+    Then we detect which services the node provides as follows:
+
+    >>> result = detect_service_instances(pub=pub, sub=sub, srv=srv)
+    >>> list(result)                                # Notice the ordering.
+    ['airspeed', 'servo', 'unrelated', '']
+    >>> result["airspeed"]                          # doctest: +NORMALIZE_WHITESPACE
+    {'foo': PortSuffixMapping(pub={'differential_pressure':  'airspeed.foo.differential_pressure',
+                                   'static_air_temperature': 'airspeed.foo.static_air_temperature'},
+                              sub={},
+                              cln={},
+                              srv={}),
+     'bar': PortSuffixMapping(pub={'differential_pressure': 'airspeed.bar.differential_pressure'},
+                              sub={'heater.state':          'airspeed.bar.heater.state'},
+                              cln={},
+                              srv={})}
+    >>> result["servo"]                             # doctest: +NORMALIZE_WHITESPACE
+    {'': PortSuffixMapping(pub={'feedback':   'servo.feedback',
+                                'status':     'servo.status',
+                                'power':      'servo.power',
+                                'dynamics':   'servo.dynamics'},
+                           sub={'setpoint':   'servo.setpoint',
+                                'readiness':  'servo.readiness'},
+                           cln={},
+                           srv={})}
+    >>> result["unrelated"]                         # doctest: +NORMALIZE_WHITESPACE
+    {'': PortSuffixMapping(pub={},
+                           sub={'subscription': 'unrelated.subscription'},
+                           cln={},
+                           srv={'server': 'unrelated.server'})}
+    >>> result[""]                                  # doctest: +NORMALIZE_WHITESPACE
+    {'': PortSuffixMapping(pub={},
+                           sub={},
+                           cln={},
+                           srv={'standalone_server': 'standalone_server'})}
+
+    As you can see, the name of a port is formed of three parts: "service_name.instance_name.suffix",
+    where the instance name can be omitted (along with its separator ".") if there is only one instance of the service.
+    If both are omitted, only the suffix remains, which can only be an application-specific port (non-standard).
+
+    The ordering is guaranteed to follow that of the input arguments.
+    """
+    out: dict[str, dict[str, PortSuffixMapping]] = {}
+
+    def psm(s: str, i: str) -> PortSuffixMapping:
+        return out.setdefault(s, {}).setdefault(i, PortSuffixMapping())
+
+    for svc, ins, suf, port in _split(pub): psm(svc, ins).pub[suf] = port
+    for svc, ins, suf, port in _split(sub): psm(svc, ins).sub[suf] = port
+    for svc, ins, suf, port in _split(cln): psm(svc, ins).cln[suf] = port
+    for svc, ins, suf, port in _split(srv): psm(svc, ins).srv[suf] = port
+
+    return out
+
+
+def _split(port_names: Iterable[str]) -> Iterable[Tuple[str, str, str, str]]:
+    for pn in port_names:
+        p = pn.split(".", 2)
+        if   len(p) > 2: yield p[0], p[1], p[2], pn
+        elif len(p) > 1: yield p[0],   "", p[1], pn
+        else:            yield   "",   "", p[0], pn
--- udral_pnp.py
+++ udral_pnp.py
@@ -3,8 +3,6 @@
 
 from __future__ import annotations
 from typing import Optional, Callable, Sequence, Tuple, AbstractSet, Type, TypeVar
-from itertools import count
-import dataclasses
 import random
 import logging
 import asyncio
@@ -21,164 +19,16 @@ import uavcan.si.sample.temperature
 import uavcan.si.sample.pressure
 import zubax.physics.dynamics.translation
 
-from .node_proxy import PortAssignment, perform_automatic_port_id_allocation
+from node_proxy import PortAssignment, perform_automatic_port_id_allocation
+from service_detector import PortSuffixMapping, detect_service_instances
 
 
 MessageClass = TypeVar("MessageClass", bound=pyuavcan.dsdl.CompositeObject)
 ServiceClass = TypeVar("ServiceClass", bound=pyuavcan.dsdl.ServiceObject)
 
 
-@dataclasses.dataclass(frozen=True)
-class PortMapping:
-    """
-    If the full port name follows the pattern ``service_name.[instance_name.]suffix``,
-    where the bracketed part is optional, this type provides the mapping from ``suffix`` to the full port name.
-    """
-    pub: dict[str, str] = dataclasses.field(default_factory=dict)
-    sub: dict[str, str] = dataclasses.field(default_factory=dict)
-    cln: dict[str, str] = dataclasses.field(default_factory=dict)
-    srv: dict[str, str] = dataclasses.field(default_factory=dict)
-
-    def match(self, cp: NonFixedPorts) -> NonFixedPorts:
-        """
-        Matches each port in the mapping with the corresponding item in the argument and returns
-        the subset of the latter. Raises :class:`KeyError` if at least one port cannot be matched.
-        """
-        return NonFixedPorts(
-            pub={k: cp.pub[k] for k in self.pub.values()},
-            sub={k: cp.sub[k] for k in self.sub.values()},
-            cln={k: cp.cln[k] for k in self.cln.values()},
-            srv={k: cp.srv[k] for k in self.srv.values()},
-        )
-
-
-def detect_service_instances(remote: NonFixedPorts,
-                             prefix: str,
-                             pub: AbstractSet[str] = frozenset(),
-                             sub: AbstractSet[str] = frozenset(),
-                             cln: AbstractSet[str] = frozenset(),
-                             srv: AbstractSet[str] = frozenset()) -> dict[str, PortMapping]:
-    """
-    This is basically the key function: it detects service instances provided by a remote node based on
-    the names of its ports.
-
-    Given a prefix and a set of port name suffixes, returns grouped ports that belong to the same service instance.
-    The caller is responsible for checking if all of the required ports are available.
-    The output instances are always lexicographically sorted by service name.
-    Ports within each instance follow the ordering defined by the remote node.
-
-    Here's a demo; suppose that the remote node has the following non-fixed ports:
-
-    >>> node_ports = NonFixedPorts(
-    ...     pub={
-    ...         "airspeed.foo.differential_pressure":   1010,
-    ...         "airspeed.foo.static_air_temperature":  1011,
-    ...         "airspeed.bar.differential_pressure":   1012,
-    ...         "servo.feedback":                       1202,
-    ...         "servo.status":                         1203,
-    ...         "servo.power":                          1204,
-    ...         "servo.dynamics":                       1205,
-    ...     },
-    ...     sub={
-    ...         "airspeed.bar.heater_state":    1000,
-    ...         "servo.setpoint":               1200,
-    ...         "servo.readiness":              1201,
-    ...         "unrelated.subscription":       2222,
-    ...     },
-    ...     srv={
-    ...         "unrelated.server": 123,
-    ...     },
-    ... )
-
-    We check if the node provides the ``airspeed`` service as follows:
-
-    >>> result = detect_service_instances(
-    ...     node_ports,
-    ...     "airspeed",     # This is the name of the service we are interested in; non-matching ports will be ignored.
-    ...     pub={           # The airspeed service specification defines these ports. Other ports will be ignored.
-    ...         "differential_pressure",
-    ...         "static_air_temperature",
-    ...     },
-    ... )
-    >>> list(result)        # Names of the matching services (sorted lexicographically).
-    ['bar', 'foo']
-    >>> result["foo"]       # The first service instance has both subjects. # doctest: +NORMALIZE_WHITESPACE
-    PortMapping(pub={'differential_pressure':  'airspeed.foo.differential_pressure',
-                     'static_air_temperature': 'airspeed.foo.static_air_temperature'},
-                sub={},
-                cln={},
-                srv={})
-    >>> result["bar"]       # This instance lacks the temperature subject. # doctest: +NORMALIZE_WHITESPACE
-    PortMapping(pub={'differential_pressure': 'airspeed.bar.differential_pressure'},
-                sub={},
-                cln={},
-                srv={})
-
-    Ports that are not part of the airspeed service are ignored
-    (they could be vendor-specific extensions or parts of other services).
-    Now, let's check if the ``servo`` service is supported by the node:
-
-    >>> result = detect_service_instances(
-    ...     node_ports,
-    ...     "servo",
-    ...     pub={           # Suppose we don't need "status" in this application.
-    ...         "feedback",
-    ...         "power",
-    ...         "dynamics",
-    ...     },
-    ...     sub={
-    ...         "setpoint",
-    ...         "readiness",
-    ...     },
-    ... )
-    >>> list(result)    # The instance is unnamed; otherwise, it would be like "servo.name.power", not "servo.power".
-    ['']
-    >>> result[""]      # doctest: +NORMALIZE_WHITESPACE
-    PortMapping(pub={'feedback':  'servo.feedback',
-                     'power':     'servo.power',
-                     'dynamics':  'servo.dynamics'},
-                sub={'setpoint':  'servo.setpoint',
-                     'readiness': 'servo.readiness'},
-                cln={},
-                srv={})
-
-    The output says that there is one instance of the servo service provided, named "" (empty).
-    As you can see, the name of a port is formed of three parts: "service_name.instance_name.suffix",
-    where the instance name can be omitted (along with its separator ".") if there is only one instance of the service.
-    """
-    def group(port_names: Sequence[str], suffixes: AbstractSet[str]) -> dict[str, list[str]]:
-        out: dict[str, list[str]] = {}
-        for pn in port_names:
-            if pn.startswith(prefix + "."):
-                for suf in suffixes:
-                    if pn.endswith("." + suf):
-                        key = pn[(len(prefix) + 1) : -(len(suf) + 1)]
-                        out.setdefault(key, []).append(suf)
-        return out
-
-    # Find all ports that match the given prefix and the given suffixes, group them by key.
-    g_pub = group(list(remote.pub), pub)
-    g_sub = group(list(remote.sub), sub)
-    g_cln = group(list(remote.cln), cln)
-    g_srv = group(list(remote.srv), srv)
-
-    # Detect which service instances that match the prefix & suffixes are available on the node.
-    # Sort them lexicographically to ensure deterministic ordering during allocation.
-    all_keys = list(sorted(set(g_pub) | set(g_sub) | set(g_cln) | set(g_srv)))
-
-    # Reconstruct port names from the groups by simply joining the three components together.
-    def make_item(k: str, suffix: str) -> Tuple[str, str]:
-        return suffix, "".join((prefix, f".{k}." if k else ".", suffix))
-
-    return {
-        key: PortMapping(
-            pub=dict(make_item(key, suffix) for suffix in g_pub.get(key, [])),
-            sub=dict(make_item(key, suffix) for suffix in g_sub.get(key, [])),
-            cln=dict(make_item(key, suffix) for suffix in g_cln.get(key, [])),
-            srv=dict(make_item(key, suffix) for suffix in g_srv.get(key, [])),
-        )
-        for key in all_keys
-    }
+def match_port_assignment(source: PortAssignment) -> PortAssignment:
+    pass
 
 
 async def main() -> None:
